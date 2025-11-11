916fc131651cc33a44dd6ac0c922dce9743ae94c
John Louie Cleofas <164037152+JLCleofas@users.noreply.github.com>
2025-11-11 15:04:06 +0800

    Created database and models

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./apardb'
+
+engine = create_engine(SQL_ALCHEMY_DATABASE_URL, connect_args={
+                       'check_same_thread': False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,21 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class AccountsPayable(Base):
+    __tablename__  = 'accountspayable'
+
+    id = Column(Integer, primary_key=True, index=True)
+    project_name = Column(String)
+    quotation = Column(String)
+    acceptance = Column(String)
+    vendor_po = Column(String)
+    supplier = Column(String)
+    document_type = Column(String)
+    invoice_number = Column(String)
+    date_paid = Column(String)
+    dv_reference = Column(String)
+    currency = Column(String)
+    po_amount = Column(String)
+    invoice_amount = Column(String)
+    balance = Column(String)
+    fully_paid = Column(Boolean, default=False)
